de7f9c1fb087de8a20faacd51a2b0f19e8293301
yakovliam <jacob@yakovliam.com>
2025-01-23 00:29:22 -0500

    feat: initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-3xl font-bold underline">
+        This is a React + TypeScript + Tailwind CSS + Vite + Bun template
+      </div>
+    </div>
+  );
+}
+
+export default App;
